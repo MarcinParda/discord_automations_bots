76eada16ebc0dd4a741bd6b3a2d2595ac844c31e
MarcinParda <marcin98parda@gmail.com>
2025-02-09 02:56:36 +0100

    refactor: Move cog imports and add_cogs function to walle.py

--- src/walle/cogs/cogs.py
+++
@@ -1,8 +0,0 @@
-from walle.cogs.guild.message import MessageEvents
-from walle.cogs.interactions.slash_commands import SlashCommands
-from walle.cogs.client.ready import ReadyCog
-
-async def add_cogs(bot):
-  await bot.add_cog(MessageEvents(bot))
-  await bot.add_cog(SlashCommands(bot))
-  await bot.add_cog(ReadyCog(bot))
--- src/walle/walle.py
+++ src/walle/walle.py
@@ -3,7 +3,14 @@ import discord
 import asyncio
 from dotenv import load_dotenv
 from discord.ext import commands
-from walle.cogs.cogs import add_cogs
+from walle.cogs.guild.message import MessageEvents
+from walle.cogs.interactions.slash_commands import SlashCommands
+from walle.cogs.client.ready import ReadyCog
+
+async def add_cogs(bot):
+  await bot.add_cog(MessageEvents(bot))
+  await bot.add_cog(SlashCommands(bot))
+  await bot.add_cog(ReadyCog(bot))
 
 load_dotenv()
 
